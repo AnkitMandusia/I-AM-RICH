8fa0c5d71556bc0249ad3561b36737e84d51c423
Ankit Mandusia <mandusiaankit.005@gmail.com>
2020-09-15 18:16:05 +0530

    Complete FlashChat

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by JPS on 08/05/20.
+//  Copyright © 2020 Ankit Mandusia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
